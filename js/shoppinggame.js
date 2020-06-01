319c0915628739368af0a284a3ff7680e2da7bb9
sumit <sumit.choudhary@eclinicalworks.com>
2020-06-01 17:26:32 -0400

    Update shoppinggame.js

--- js/shoppinggame.js
+++ js/shoppinggame.js
@@ -46,7 +46,11 @@ const dateDiff = (date1, date2) => {
 };
 
 // Here, use Object.defineProperty to create property - daysToExpire
-Object.defineProperty(Product.prototype,'daysToExpire',{})
+Object.defineProperty(Product.prototype,'daysToExpire',{
+    get: function ()  {
+        return dateDiff(this.expiryDate,new Date());
+    }
+})
 
 // Add method getDetails to Product here
 
