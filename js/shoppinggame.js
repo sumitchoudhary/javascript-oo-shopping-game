a83e3509d2d153bc37fe083e5f1201b39a0436b7
sumit <sumit.choudhary@eclinicalworks.com>
2020-06-01 16:51:16 -0400

    Update shoppinggame.js

--- js/shoppinggame.js
+++ js/shoppinggame.js
@@ -26,6 +26,15 @@ let player = {
 
 // Define the Product class - write the Constructor function for Product class here
 
+function Product (id,name,price,expiryDate){
+this.id = id;
+this.name = name;
+this.price = price;
+this.expiryDate = expiryDate;
+
+};
+
+
 // Complete the dateDiff function
 const dateDiff = (date1, date2) => {};
 
